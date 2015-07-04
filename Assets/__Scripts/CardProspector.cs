c5282d6a999d7acde19a82e0714b31e19900436c
Bill Crosbie <crosbie.git@gmail.com>
2015-07-04 16:34:23 -0400

    Set up Card prospector
    Don’t forget to change the prefab Card script from Card to
    CardProspector. Failure to do this step will mean that the deck will
    have nothing but null items in it.

---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+// these are the only places a card should be in the game
+// setting this up for convenience in naming
+public enum CardState {
+	drawpile,
+	tableau,
+	target,
+	discard
+}
+
+
+// Note this extends Card, not MonoBehavior
+public class CardProspector : Card {
+
+	public CardState			state = CardState.drawpile;	
+	public List<CardProspector>	hiddenBy = new List<CardProspector>();  //which cards keep this face down
+	public int					layoutID;		// matches to layout if in tableau
+	public SlotDef				slotDef;		// data pulled from layout.xml 
+
+
+}  // CardProspector
--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -9,8 +9,10 @@ public class Prospector : MonoBehaviour {
 	public Deck					deck;
 	public TextAsset			deckXML;
 	
-	public Layout				layout;
+	public Layout				layout;		// p 589
 	public TextAsset			layoutXML;
+	
+	public List<CardProspector> drawPile;
 
 	void Awake(){
 		S = this;
@@ -21,8 +23,25 @@ public class Prospector : MonoBehaviour {
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle (ref deck.cards);
 		
+		// added p 589
 		layout = GetComponent<Layout>();
 		layout.ReadLayout(layoutXML.text);
+		
+		drawPile = ConvertListCardsToListCardProspector(deck.cards);
 	}
+
+
+	// A utility function designed to take Cards in and make them
+	// CardProspector's. Returns the deck, converted ready for game play
+	List<CardProspector> ConvertListCardsToListCardProspector(List<Card> lCD){
+		List<CardProspector> lCP = new List<CardProspector>();
+		CardProspector tCP;
+		
+		foreach(Card tCD in lCD){
+			tCP = tCD as CardProspector;
+			lCP.Add (tCP);
+		}
+		return(lCP);
+	} // ConvertListCardsToListCardProspector
 
 }
