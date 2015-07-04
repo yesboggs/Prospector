6623bf322dc3ba0d8ca312227558833e75daee72
Bill Crosbie <crosbie.git@gmail.com>
2015-07-04 17:08:01 -0400

    Draw & LayoutGame
    pages 592-3 in Gibson

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -12,6 +12,16 @@ public class Prospector : MonoBehaviour {
 	public Layout				layout;		// p 589
 	public TextAsset			layoutXML;
 	
+	public Vector3				layoutCenter;
+	public float				xoffset = 3f;
+	public float				yoffset = -2.5f;
+	public Transform			layoutAnchor;
+	
+	public CardProspector		target;
+	public List<CardProspector>	tableau;
+	public List<CardProspector>	discardPile;
+	
+	
 	public List<CardProspector> drawPile;
 
 	void Awake(){
@@ -28,6 +38,7 @@ public class Prospector : MonoBehaviour {
 		layout.ReadLayout(layoutXML.text);
 		
 		drawPile = ConvertListCardsToListCardProspector(deck.cards);
+		LayoutGame();
 	}
 
 
@@ -43,5 +54,47 @@ public class Prospector : MonoBehaviour {
 		}
 		return(lCP);
 	} // ConvertListCardsToListCardProspector
+	
+	
+	// Draw() - draw a single card from the drawPile and returns it
+	CardProspector Draw(){
+		CardProspector cd = drawPile[0];
+		drawPile.RemoveAt(0);
+		return (cd);
+	} // Draw
 
-}
+
+	// LayoutGame() - positions the original tableau of cards
+	void LayoutGame(){
+	
+		// first create an empty game object to anchor the tableau
+		if (layoutAnchor == null) {
+			GameObject tGO = new GameObject("_LayoutAnchor");
+			layoutAnchor = tGO.transform;
+			layoutAnchor.transform.position = layoutCenter;
+		}	
+		
+		// then position all of the cards on the basis of the data from layoutXML
+		CardProspector cp;
+		
+		foreach(SlotDef tSD in layout.slotDefs) {
+			cp = Draw ();
+			cp.faceUP = tSD.faceUp;
+			cp.transform.parent = layoutAnchor;  // move from deck to tableau in hierarchy
+			
+			cp.transform.localPosition = new Vector3(
+				layout.multiplier.x * tSD.x,
+				layout.multiplier.y * tSD.y,
+				-tSD.layerID ); // end new Vector3
+				
+			cp.layoutID = tSD.id;
+			cp.slotDef = tSD;
+			cp.state = CardState.tableau;
+			
+			tableau.Add (cp);	
+			
+		} //foreach SlotDef
+				
+	} //LayoutGame
+
+} // Prospector
